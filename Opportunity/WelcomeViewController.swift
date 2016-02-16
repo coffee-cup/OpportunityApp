9bd0294a81a6a5ef1891a926464dd3653f9827f2
Jake Runzer <jakerunzer@gmail.com>
2016-02-16 00:40:42 -0800

    Initial Commit

---
+++ Opportunity/WelcomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WelcomeViewController.swift
+//  Opportunity
+//
+//  Created by Jake Runzer on 2016-02-16.
+//  Copyright © 2016 FixCode. All rights reserved.
+//
+
+import Cocoa
+
+class WelcomeViewController: UIViewController {
+
+}
